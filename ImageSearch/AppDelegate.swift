40fce766102a5694d9c391f0121fdd3340cbfb44
Vladyslav Petrenko <vladyslav533p@icloud.com>
2023-06-11 18:13:16 +0200

    Initial commit

---
+++ ImageSearch/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  ImageSearch
+//
+//  Created by Vladyslav Petrenko on 11/06/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if #unavailable(iOS 13.0) {
+            window = UIWindow()
+            let navigationController = UINavigationController(rootViewController: SearchView())
+            window?.rootViewController = navigationController
+            window?.makeKeyAndVisible()
+        }
+        return true
+    }
+}
+
---
+++ ImageSearch/Views/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchView.swift
+//  ImageSearch
+//
+//  Created by Vladyslav Petrenko on 11/06/2023.
+//
+
+import UIKit
+
+class SearchView: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
